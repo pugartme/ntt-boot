53750609849276734a3b35d8ae588baaad1b9f3b
Paulo <antoniougartemendoza@gmail.com>
2026-07-01 16:32:04 -0500

    text min added

--- token/src/main/java/com/lab/expose/GreetingResource.java
+++ token/src/main/java/com/lab/expose/GreetingResource.java
@@ -11,7 +11,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        System.out.println("Hello arrived");
+        System.out.println("Hello arrived!");
         return "Hello from Quarkus REST In Auth-Service";
     }
 }
